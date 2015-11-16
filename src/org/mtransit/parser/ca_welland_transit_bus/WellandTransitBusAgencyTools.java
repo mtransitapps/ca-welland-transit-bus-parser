3effd08f001f76544ca44181d6a1d780e5d2f118
Mathieu Méa <mathieu.mea@gmail.com>
2015-11-16 11:17:32 -0500

    Better filtering of service IDs for multi-agencies GTFS files.

--- src/org/mtransit/parser/ca_welland_transit_bus/WellandTransitBusAgencyTools.java
+++ src/org/mtransit/parser/ca_welland_transit_bus/WellandTransitBusAgencyTools.java
@@ -49,7 +49,7 @@ public class WellandTransitBusAgencyTools extends DefaultAgencyTools {
 	public void start(String[] args) {
 		System.out.printf("\nGenerating Welland Transit bus data...");
 		long start = System.currentTimeMillis();
-		this.serviceIds = extractUsefulServiceIds(args, this);
+		this.serviceIds = extractUsefulServiceIds(args, this, true);
 		super.start(args);
 		System.out.printf("\nGenerating Welland Transit bus data... DONE in %s.\n", Utils.getPrettyDuration(System.currentTimeMillis() - start));
 	}
