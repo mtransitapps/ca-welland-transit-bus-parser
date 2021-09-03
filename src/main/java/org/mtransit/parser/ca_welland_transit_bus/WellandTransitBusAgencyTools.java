905211623d73e71e0992230dcdc3905715e65f96
Mathieu Méa <mathieu.mea@gmail.com>
2021-09-03 10:40:55 -0400

    New URL

--- src/main/java/org/mtransit/parser/ca_welland_transit_bus/WellandTransitBusAgencyTools.java
+++ src/main/java/org/mtransit/parser/ca_welland_transit_bus/WellandTransitBusAgencyTools.java
@@ -1,106 +1,65 @@
 package org.mtransit.parser.ca_welland_transit_bus;
 
+import static org.mtransit.parser.StringUtils.EMPTY;
+
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
-import org.mtransit.parser.CleanUtils;
+import org.mtransit.commons.CharUtils;
+import org.mtransit.commons.CleanUtils;
 import org.mtransit.parser.DefaultAgencyTools;
 import org.mtransit.parser.MTLog;
-import org.mtransit.parser.Utils;
-import org.mtransit.parser.gtfs.data.GCalendar;
-import org.mtransit.parser.gtfs.data.GCalendarDate;
 import org.mtransit.parser.gtfs.data.GRoute;
-import org.mtransit.parser.gtfs.data.GSpec;
 import org.mtransit.parser.gtfs.data.GStop;
-import org.mtransit.parser.gtfs.data.GTrip;
 import org.mtransit.parser.mt.data.MAgency;
-import org.mtransit.parser.mt.data.MRoute;
-import org.mtransit.parser.mt.data.MTrip;
 
-import java.util.HashSet;
 import java.util.Locale;
-import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-
-import static org.mtransit.parser.StringUtils.EMPTY;
 
 // http://www.niagararegion.ca/government/opendata/data-set.aspx#id=32
 // http://www.niagararegion.ca/government/opendata/data-set.aspx#id=32&tab=data_table&f=xml&r=500&p=1
 // http://maps-dev.niagararegion.ca/GoogleTransit/NiagaraRegionTransit.zip
 // https://www.niagararegion.ca/downloads/transit/NiagaraRegionTransit.zip
+// https://niagaraopendata.ca/dataset/1a1b885e-1a86-415d-99aa-6803a2d8f178/resource/52c8cd46-d976-4d57-990f-e8018bcd27cb/download/gtfs.zip
 public class WellandTransitBusAgencyTools extends DefaultAgencyTools {
 
-	public static void main(@Nullable String[] args) {
-		if (args == null || args.length == 0) {
-			args = new String[3];
-			args[0] = "input/gtfs.zip";
-			args[1] = "../../mtransitapps/ca-welland-transit-bus-android/res/raw/";
-			args[2] = ""; // files-prefix
-		}
+	public static void main(@NotNull String[] args) {
 		new WellandTransitBusAgencyTools().start(args);
 	}
 
-	@Nullable
-	private HashSet<Integer> serviceIdInts;
-
 	@Override
-	public void start(@NotNull String[] args) {
-		MTLog.log("Generating Welland Transit bus data...");
-		long start = System.currentTimeMillis();
-		this.serviceIdInts = extractUsefulServiceIdInts(args, this, true);
-		super.start(args);
-		MTLog.log("Generating Welland Transit bus data... DONE in %s.", Utils.getPrettyDuration(System.currentTimeMillis() - start));
+	public boolean defaultExcludeEnabled() {
+		return true;
 	}
 
+	@NotNull
 	@Override
-	public boolean excludingAll() {
-		return this.serviceIdInts != null && this.serviceIdInts.isEmpty();
-	}
-
-	@Override
-	public boolean excludeCalendar(@NotNull GCalendar gCalendar) {
-		if (this.serviceIdInts != null) {
-			return excludeUselessCalendarInt(gCalendar, this.serviceIdInts);
-		}
-		return super.excludeCalendar(gCalendar);
-	}
-
-	@Override
-	public boolean excludeCalendarDate(@NotNull GCalendarDate gCalendarDates) {
-		if (this.serviceIdInts != null) {
-			return excludeUselessCalendarDateInt(gCalendarDates, this.serviceIdInts);
-		}
-		return super.excludeCalendarDate(gCalendarDates);
-	}
-
-	@Override
-	public boolean excludeTrip(@NotNull GTrip gTrip) {
-		if (this.serviceIdInts != null) {
-			return excludeUselessTripInt(gTrip, this.serviceIdInts);
-		}
-		return super.excludeTrip(gTrip);
+	public String getAgencyName() {
+		return "Welland Transit";
 	}
 
 	@Override
 	public boolean excludeRoute(@NotNull GRoute gRoute) {
 		//noinspection deprecation
-		final String agencyId = gRoute.getAgencyId();
+		final String agencyId = gRoute.getAgencyIdOrDefault();
 		if (!agencyId.startsWith("WE_") //
 				&& !agencyId.startsWith("Wel_") //
 				&& !agencyId.startsWith("WEL_") //
-				&& !agencyId.contains("AllNRT_")) {
-			return true; // exclude
+				&& !agencyId.contains("AllNRT_")
+				&& !agencyId.equals("1")) {
+			return EXCLUDE;
 		}
-		if (agencyId.contains("AllNRT_")) {
-			if (!Utils.isDigitsOnly(gRoute.getRouteShortName())) {
-				return true; // exclude
+		if (agencyId.contains("AllNRT_") || agencyId.equals("1")) {
+			final String rsnS = gRoute.getRouteShortName();
+			if (!CharUtils.isDigitsOnly(rsnS)) {
+				return EXCLUDE;
 			}
-			final int rsn = Integer.parseInt(gRoute.getRouteShortName());
+			final int rsn = Integer.parseInt(rsnS);
 			if (rsn < 500 || rsn > 799) { // includes Port Colborne for now
-				return true; // exclude
+				return EXCLUDE;
 			}
 		}
-		if (gRoute.getRouteLongName().startsWith("NRT - ")) {
-			return true; // exclude Niagara Region Transit buses
+		if (gRoute.getRouteLongNameOrDefault().startsWith("NRT - ")) {
+			return EXCLUDE; // Niagara Region Transit buses
 		}
 		return super.excludeRoute(gRoute);
 	}
@@ -113,8 +72,10 @@ public class WellandTransitBusAgencyTools extends DefaultAgencyTools {
 
 	@Override
 	public long getRouteId(@NotNull GRoute gRoute) {
-		if (gRoute.getRouteShortName() != null && gRoute.getRouteShortName().length() > 0 && Utils.isDigitsOnly(gRoute.getRouteShortName())) {
-			return Long.parseLong(gRoute.getRouteShortName()); // using route short name as route ID
+		final String rsnS = gRoute.getRouteShortName();
+		if (!rsnS.isEmpty()
+				&& CharUtils.isDigitsOnly(rsnS)) {
+			return Long.parseLong(rsnS); // using route short name as route ID
 		}
 		throw new MTLog.Fatal("Unexpected route ID for %s!", gRoute);
 	}
@@ -140,8 +101,10 @@ public class WellandTransitBusAgencyTools extends DefaultAgencyTools {
 			if ("College-Brock".equals(gRoute.getRouteId())) {
 				return BL;
 			} else {
-				if (gRoute.getRouteShortName() != null && gRoute.getRouteShortName().length() > 0 && Utils.isDigitsOnly(gRoute.getRouteShortName())) {
-					return gRoute.getRouteShortName();
+				final String rnsS = gRoute.getRouteShortName();
+				if (!rnsS.isEmpty()
+						&& CharUtils.isDigitsOnly(rnsS)) {
+					return rnsS;
 				}
 			}
 		throw new MTLog.Fatal("Unexpected route short name for %s!", gRoute);
@@ -155,8 +118,7 @@ public class WellandTransitBusAgencyTools extends DefaultAgencyTools {
 
 	@NotNull
 	@Override
-	public String getRouteLongName(@NotNull GRoute gRoute) {
-		String routeLongName = gRoute.getRouteLongName();
+	public String cleanRouteLongName(@NotNull String routeLongName) {
 		routeLongName = POINT.matcher(routeLongName).replaceAll(POINT_REPLACEMENT);
 		routeLongName = POINTS.matcher(routeLongName).replaceAll(POINTS_REPLACEMENT);
 		routeLongName = CleanUtils.cleanNumbers(routeLongName);
@@ -178,10 +140,10 @@ public class WellandTransitBusAgencyTools extends DefaultAgencyTools {
 	@Nullable
 	@Override
 	public String getRouteColor(@NotNull GRoute gRoute) {
-		if (gRoute.getRouteShortName() != null
-				&& gRoute.getRouteShortName().length() > 0
-				&& Utils.isDigitsOnly(gRoute.getRouteShortName())) {
-			int rsn = Integer.parseInt(gRoute.getRouteShortName());
+		final String rsnS = gRoute.getRouteShortName();
+		if (!rsnS.isEmpty()
+				&& CharUtils.isDigitsOnly(rsnS)) {
+			final int rsn = Integer.parseInt(rsnS);
 			switch (rsn) {
 			// @formatter:off
 			case 23: return "2B6ABC";
@@ -219,21 +181,8 @@ public class WellandTransitBusAgencyTools extends DefaultAgencyTools {
 	}
 
 	@Override
-	public void setTripHeadsign(@NotNull MRoute mRoute, @NotNull MTrip mTrip, @NotNull GTrip gTrip, @NotNull GSpec gtfs) {
-		mTrip.setHeadsignString(
-				cleanTripHeadsign(gTrip.getTripHeadsign()),
-				gTrip.getDirectionId()
-		);
-	}
-
-	@Override
 	public boolean directionFinderEnabled() {
 		return true;
-	}
-
-	@Override
-	public boolean mergeHeadsign(@NotNull MTrip mTrip, @NotNull MTrip mTripToMerge) {
-		throw new MTLog.Fatal("Unexpected trips to merge %s & %s!", mTrip, mTripToMerge);
 	}
 
 	@NotNull
@@ -289,7 +238,7 @@ public class WellandTransitBusAgencyTools extends DefaultAgencyTools {
 	@Override
 	public String getStopCode(@NotNull GStop gStop) {
 		String stopCode = gStop.getStopCode();
-		if (stopCode == null || stopCode.length() == 0 || ZERO_0.equals(stopCode)) {
+		if (stopCode.length() == 0 || ZERO_0.equals(stopCode)) {
 			//noinspection deprecation
 			stopCode = gStop.getStopId();
 		}
@@ -299,112 +248,14 @@ public class WellandTransitBusAgencyTools extends DefaultAgencyTools {
 
 	@Override
 	public int getStopId(@NotNull GStop gStop) {
-		if (true) {
-			//noinspection deprecation
-			String stopId = gStop.getStopId();
-			stopId = STARTS_WITH_WE_A00_.matcher(stopId).replaceAll(EMPTY);
-			if (stopId.isEmpty()) {
-				throw new MTLog.Fatal("Unexpected stop ID (%d) %s!", stopId, gStop.toStringPlus());
-			}
-			if (Utils.isDigitsOnly(stopId)) {
-				return Integer.parseInt(stopId);
-			}
-			throw new MTLog.Fatal("Unexpected stop ID %s!", gStop.toStringPlus());
+		//noinspection deprecation
+		String stopId = gStop.getStopId();
+		stopId = STARTS_WITH_WE_A00_.matcher(stopId).replaceAll(EMPTY);
+		if (stopId.isEmpty()) {
+			throw new MTLog.Fatal("Unexpected stop ID (%d) %s!", stopId, gStop.toStringPlus());
 		}
-		String stopCode = gStop.getStopCode();
-		if (stopCode == null || stopCode.length() == 0 || ZERO_0.equals(stopCode)) {
-			//noinspection deprecation
-			stopCode = gStop.getStopId();
-		}
-		stopCode = STARTS_WITH_WE_A00_.matcher(stopCode).replaceAll(EMPTY);
-		if (Utils.isDigitsOnly(stopCode)) {
-			return Integer.parseInt(stopCode); // using stop code as stop ID
-		}
-		switch (stopCode) {
-		case "AQE":
-			return 9_000_000;
-		case "BRC":
-			return 9_000_001;
-		case "BRU":
-			return 9_000_002;
-		case "EMW":
-			return 9_000_003;
-		case "FIF":
-			return 9_000_004;
-		case "FIN":
-			return 9_000_005;
-		case "FIO":
-			return 9_000_006;
-		case "GlndCmps":
-			return 9_000_007;
-		case "HAW":
-			return 9_000_008;
-		case "HOW":
-			return 9_000_009;
-		case "KIO":
-			return 9_000_010;
-		case "LIO":
-			return 9_000_011;
-		case "NCC":
-			return 9_000_012;
-		case "NIE":
-			return 9_000_013;
-		case "ONC":
-			return 9_000_014;
-		case "ONS":
-			return 9_000_015;
-		case "PCH":
-			return 9_000_016;
-		case "PRW":
-			return 9_000_017;
-		case "SewayMal":
-			return 9_000_018;
-		case "SGR":
-			return 9_000_019;
-		case "SPM":
-			return 9_000_020;
-		case "THB":
-			return 9_000_021;
-		case "THN":
-			return 9_000_022;
-		case "Welland":
-			return 9_000_023;
-		case "WlndCamp":
-			return 9_000_024;
-		case "WOR":
-			return 9_000_025;
-		case "WOS":
-			return 9_000_026;
-		case "WPC":
-			return 9_000_027;
-		case "WWA":
-			return 9_000_028;
-		case "LIP":
-			return 9_000_029;
-		case "XXXX":
-			return 9_000_030;
-		}
-		try {
-			Matcher matcher = DIGITS.matcher(stopCode);
-			if (matcher.find()) {
-				int routeId = Integer.parseInt(matcher.group());
-				if (stopCode.startsWith(NOTL)) {
-					routeId += 14_000_000;
-				} else if (stopCode.startsWith(PC2)) {
-					routeId += 160_000_000;
-				} else if (stopCode.startsWith(PC)) {
-					routeId += 161_000_000;
-				} else if (stopCode.startsWith(WE2)) {
-					routeId += 233_000_000;
-				} else if (stopCode.startsWith(WE)) {
-					routeId += 234_000_000;
-				} else {
-					throw new MTLog.Fatal("Unexpected stop ID (starts with digits) %s!", gStop.toStringPlus());
-				}
-				return routeId;
-			}
-		} catch (Exception e) {
-			throw new MTLog.Fatal(e, "Error while finding stop ID for %s!", gStop.toStringPlus());
+		if (CharUtils.isDigitsOnly(stopId)) {
+			return Integer.parseInt(stopId);
 		}
 		throw new MTLog.Fatal("Unexpected stop ID %s!", gStop.toStringPlus());
 	}
